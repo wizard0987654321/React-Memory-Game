5674ac76b7bb47f0fabd09a742d845a6728c6362
Givi Sabashvili <givisab12345@gmail.com>
2024-08-16 21:51:32 +0200

    Test Commit, MemoryGame

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import MemoryGame from './MemoryGame.jsx'
+
+function App() {
+  return (
+    <>
+      <MemoryGame />
+    </>
+  )  
+}
+
+export default App
---
+++ vite-project/src/MemoryGame.jsx
@@ -0,0 +1,7 @@
+export default function MemoryGame() {
+    return (
+        <div>
+            <h1>React Componentiii</h1>
+        </div>
+    );
+}
